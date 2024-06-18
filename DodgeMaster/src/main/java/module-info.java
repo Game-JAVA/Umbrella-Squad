b4d53ba286c8f03f32e23d4f08186f1659abb5e2
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-06-18 14:01:50 -0300

    feat: added initial structure

---
+++ DodgeMaster/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.dodgemaster {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.dodgemaster to javafx.fxml;
+    exports org.example.dodgemaster;
+}
