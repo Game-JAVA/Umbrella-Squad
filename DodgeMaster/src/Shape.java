3cab18b3169d1f244fcbc9b157932a2b0c15c91b
b4rbosa777 <matheushb756@gmail.com>
2024-06-19 15:47:16 -0300

    docs/matheus_finish_class_shape

--- DodgeMaster/src/Shape.java
+++ DodgeMaster/src/Shape.java
@@ -1,4 +1,6 @@
-public class Shape {
+import java.util.Random;
+
+public abstract class Shape {
     //Atributos
 
     private int posX;
@@ -9,24 +11,69 @@ public class Shape {
 
     private int speedY;
 
+    Random r = new Random();
 
     //Construtor
 
     public Shape() {
-        this.posX = posX;
-        this.posY = posY;
-        this.speedX = speedX;
-        this.speedY = speedY;
+        this.posX = posX;   // Por enquanto nao será implementado por precisar do tamanhdo da tela.
+        this.posY = posY;   // Por enquanto nao será implementado por precisar do tamanhdo da tela.
+        this.speedX = 5;
+        this.speedY = 5;
     }
 
 
     //Métodos
 
-    public static void move(int screenHeightX,int screenWidth){
-
+    public  void move(int screenHeightX,int screenWidth){
+        this.posX += speedX;
+        this.posY += speedY;
     }
     //Getters and Seters
 
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
 
     //ToString
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "posX=" + posX +
+                ", posY=" + posY +
+                ", speedX=" + speedX +
+                ", speedY=" + speedY +
+                '}';
+    }
 }
