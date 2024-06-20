fc1f1188dd05854d1b5de062f03b4b3c0837d4f2
tonas <thomas11ventura@gmail.com>
2024-06-20 14:50:10 -0300

    feat: main script and window chore

--- DodgeMaster/src/Main.java
+++ DodgeMaster/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Dodge Master");
+        frame.setMinimumSize(new Dimension(1360, 768)); // Minimum window size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // Terminate window process when closed
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);               // Starts as windowed full-screen
+        frame.setVisible(true);                                      // Displays the window
     }
 }
