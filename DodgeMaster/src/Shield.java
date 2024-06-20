11673ea3e0ba30f757d9a9a78dc056b8137d88b7
b4rbosa777 <matheushb756@gmail.com>
2024-06-20 17:41:04 -0300

    feat: developing_class_shield

---
+++ DodgeMaster/src/Shield.java
@@ -0,0 +1,16 @@
+public class Shield extends Rectangle {
+    //Attributes
+ private int health;
+ private int diameter;
+
+    //Constructor
+
+    public Shield() {
+        this.health = health;
+        this.diameter = diameter;
+    }
+
+
+    //Methods
+
+}
