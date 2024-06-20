4734b2e501d1ab91426f2b08998af988f7521ab8
tonas <thomas11ventura@gmail.com>
2024-06-20 13:23:01 -0300

    perf: commenting, handling whitespace and overloads in the classes

--- DodgeMaster/src/Rectangle.java
+++ DodgeMaster/src/Rectangle.java
@@ -1,63 +1,55 @@
+import java.awt.*;
+
 public class Rectangle extends Shape {
-    //Atributos
+    // Attributes
     private int width;
     private int height;
 
-    //Construtor
-
-    public Rectangle () {
-        this.width = 10;
-        this.height = 10; //tamanho nao definido ainda
-    }
-
-
-
-    //Métodos
-
-    @Override
-    public void move (int screenWidth, int screenHeight) {
-        super.move(screenWidth, screenHeight);
-        // Collision treatment
-
-        if(super.getPosX() < 0 || super.getPosX() + this.width > screenWidth) {
-            super.setSpeedX(super.getSpeedX() * -1);
-        }
-
-        if(super.getPosY() < 0 || super.getPosY() + this.height > screenHeight) {
-            super.setSpeedY(super.getSpeedY() * -1);
-        }
-
-    }
-
-
-    //Getter and Setter
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
+    // Constructor {
+    public Rectangle(int x, int y, int speedX, int speedY, int width, int height) {
+        super(x, y, speedX, speedY);
         this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
         this.height = height;
     }
 
+        // No signatures overload
+    public Rectangle() {
+        this.width = 100;
+        this.height = 100;
+    }
+    // }
+
+    // Methods {
+    @Override
+    public void move(int screenWidth, int screenHeight) {
+        super.move(screenWidth, screenHeight);
+
+        // Collision Treatment
+        if (super.getX() < 0 || super.getX() + this.width > screenWidth)
+            super.setSpeedX(super.getSpeedX() * -1);
+        if (super.getY() < 0 || super.getY() + this.height > screenHeight)
+            super.setSpeedY(super.getSpeedY() * -1);
+    }
+
+    public void draw(Graphics g) {
+        // *Still need to figure out how to import the sprite*
+        g.fillRect(super.getX(), super.getY(), this.width, this.height);
+    }
+    // }
+
+    // Getters and Setters
+    public int getWidth() {return width;}
+    public int getHeight() {return height;}
+
+    public void setWidth(int width) {this.width = width;}
+    public void setHeight(int height) {this.height = height;}
 
     //ToString
-
-
     @Override
     public String toString() {
-        return "Rectangle{" +
+        return super.toString() + " Rectangle{" +
                 "width=" + width +
                 ", height=" + height +
-                '}'+
-                super.toString();
+                '}';
     }
 }
--- DodgeMaster/src/Shape.java
+++ DodgeMaster/src/Shape.java
@@ -1,77 +1,77 @@
+import java.awt.*;
 import java.util.Random;
 
 public abstract class Shape {
-    //Atributos
-
-    private int posX;
-
-    private int posY;
-
+    // Attributes
+    private int x;
+    private int y;
     private int speedX;
-
     private int speedY;
 
-    Random r = new Random();
+    // Constructors {
+    public Shape(int x, int y, int speedX, int speedY) {
+        this.x = x;
+        this.y = y;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
 
-    //Construtor
-
+        // No signatures overload
     public Shape() {
-        this.posX = posX;   // Por enquanto nao será implementado por precisar do tamanhdo da tela.
-        this.posY = posY;   // Por enquanto nao será implementado por precisar do tamanhdo da tela.
-        this.speedX = 5;
-        this.speedY = 5;
+        this.x = 100;
+        this.y = 100;
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+    // }
+
+    // Methods {
+    public void move() {
+        this.x += speedX;
+        this.y += speedY;
+    }
+    // Signatures to handle collision treatment
+    protected void move(int screenWidth, int screenHeight) {
+        this.x += speedX;
+        this.y += speedY;
     }
 
+    public abstract void draw(Graphics g);
+    // }
 
-    //Métodos
-
-    public  void move(int screenHeight,int screenWidth){
-        this.posX += speedX;
-        this.posY += speedY;
+    // Getters and Setters
+    public int getX() {
+        return x;
     }
-    //Getters and Setters
-
-    public int getPosX() {
-        return posX;
+    public int getY() {
+        return y;
     }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public int getPosY() {
-        return posY;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
-
     public int getSpeedX() {
         return speedX;
     }
-
-    public void setSpeedX(int speedX) {
-        this.speedX = speedX;
-    }
-
     public int getSpeedY() {
         return speedY;
     }
 
+    public void setX(int posX) {
+        this.x = posX;
+    }
+    public void setY(int posY) {
+        this.y = posY;
+    }
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
     public void setSpeedY(int speedY) {
         this.speedY = speedY;
     }
 
-
     //ToString
-
-
     @Override
     public String toString() {
         return "Shape{" +
-                "posX=" + posX +
-                ", posY=" + posY +
+                "posX=" + x +
+                ", posY=" + y +
                 ", speedX=" + speedX +
                 ", speedY=" + speedY +
                 '}';
