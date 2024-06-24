93fdef09242bee83082b9d49d56f1df2f9ccdbc5
tonas <thomas11ventura@gmail.com>
2024-06-24 17:19:05 -0300

    refactor: remade the main to better performance

--- DodgeMaster/src/Main.java
+++ DodgeMaster/src/Main.java
@@ -1,46 +1,113 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Dodge Master");
-        Player david = new Player(0,0,0,0,100,100,3,10,"../assets/david_sprite_01.png");
+@SuppressWarnings("serial")
+public class Main extends javax.swing.JFrame implements Runnable{
+    // Attributes {
+        // Frame size
+    private int width = 1360;
+    private int height = 768;
+        // Background
+    private ImagePanel backgroundPanel;
+        // Keys (When the keyboard is pressed, the respective key is set true)
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+    // }
 
-        // Setting the game window {
-        // Creating the main frame aka the game window
-        frame.setMinimumSize(new Dimension(1360, 768)); // Minimum window size
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // Terminate window process when closed
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);               // Starts as windowed full-screen
-        frame.setContentPane(new ImagePanel("../assets/bg_city.png"));  // Setting the background
-        // Adding the player to the scene
-        frame.setLayout(null);
-        frame.add(david.getPlayerPanel());
-        // Makes the frame visible
-        frame.setVisible(true);
-        // }
+    // Constructor
+    public Main() {
+        // Method to fetch initial setup
+        initComponents();
 
+        // Frame config
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);  // Starts at windowed maximized
+        setLayout(null); // Absolute Layout
+        setContentPane(new ImagePanel("../assets/bg_city.png"));
 
-        // Game loop screen updater {
-        // 16 milliseconds to approach 60 fps
-        new Timer(16, e -> {
-            david.move(frame.getWidth(), frame.getHeight()); // Updates the player position
-            david.getPlayerPanel().repaint(); // Redraw the player in the frame
-        }).start();
-        // }
-
-        // Player interaction {
-        // Adding key listeners
-        frame.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {david.keyPressed(e);}
-            @Override
-            public void keyReleased(KeyEvent e) {david.keyRelease(e);}
+        // Keyboard listener
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {formKeyPressed(evt);}
+            public void keyReleased(java.awt.event.KeyEvent evt) {formKeyReleased(evt);}
         });
 
-        // Focus the JFrame to be able to capture the keyboard
-        frame.requestFocus();
-        // }
+        setVisible(true);
+        // Buffering
+        createBufferStrategy(2);
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    private void initComponents() {
+        setMinimumSize(new Dimension(width, height));
+    }
+
+    @SuppressWarnings("unchecked")
+    // Verifying key press
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {left = true;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {right = true;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_UP) {up = true;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_DOWN) {down = true;}
+    }//GEN-LAST:event_formKeyPressed
+
+    // Verifying key release
+    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
+        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {left = false;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {right = false;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_UP) {up = false;}
+        /* else */ if (evt.getKeyCode() == KeyEvent.VK_DOWN) {down = false;}
+    }//GEN-LAST:event_formKeyReleased
+
+    // Initiate the window
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+
+    public void run() {
+        Graphics g = getBufferStrategy().getDrawGraphics();
+        // Objects:
+
+
+
+        //
+        while(true) {
+            // Updates g
+            g = getBufferStrategy().getDrawGraphics();
+            // Clear the screen
+            g.clearRect(0, 0, getWidth(), getHeight());
+            // Runtime:
+
+
+            // Appends the screen
+            getBufferStrategy().show();
+
+            // Buffer in 16 milliseconds to approach the 60 fps
+            try {Thread.sleep(16);} catch (InterruptedException ex) {ex.printStackTrace();}
+        }
     }
 }
