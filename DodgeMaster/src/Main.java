a8610f71758404b91ab2cf8f22eb66f7f761df10
tonas <thomas11ventura@gmail.com>
2024-06-26 14:59:56 -0300

    chore: refactored main and player, movement still buggy

--- DodgeMaster/src/Main.java
+++ DodgeMaster/src/Main.java
@@ -1,23 +1,14 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.image.BufferStrategy;
 
-@SuppressWarnings("serial")
 public class Main extends javax.swing.JFrame implements Runnable{
     // Attributes {
         // Frame size
     private int width = 1360;
     private int height = 768;
-        // Background panel
+        // Entities
     private ImagePanel backgroundPanel;
-        // Keys (When the keyboard is pressed, the respective key is set true)
-    private boolean left;
-    private boolean right;
-    private boolean up;
-    private boolean down;
+    private Player player;
     // }
-
 
     // Constructor
     public Main() {
@@ -26,8 +17,12 @@ public class Main extends javax.swing.JFrame implements Runnable{
 
         // Keyboard listener
         addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {formKeyPressed(evt);}
-            public void keyReleased(java.awt.event.KeyEvent evt) {formKeyReleased(evt);}
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                player.keyPressed(evt);
+            }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                player.keyRelease(evt);
+            }
         });
 
         setVisible(true);
@@ -41,73 +36,21 @@ public class Main extends javax.swing.JFrame implements Runnable{
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(width, height));
         backgroundPanel = new ImagePanel("../assets/bg_city.png");
+        player = new Player(100, 100, 0, 0, 50, 50, 100, 2, "../assets/david_sprite_01.png");
+        backgroundPanel.setLayout(null);  // Allows for absolute positioning
+        backgroundPanel.add(player.getPlayerPanel());
         setContentPane(backgroundPanel);
         pack();
     }
 
-    @SuppressWarnings("unchecked")
-    // Verifying key press
-    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {left = true;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {right = true;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_UP) {up = true;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_DOWN) {down = true;}
-    }//GEN-LAST:event_formKeyPressed
-
-    // Verifying key release
-    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
-        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {left = false;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {right = false;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_UP) {up = false;}
-        /* else */ if (evt.getKeyCode() == KeyEvent.VK_DOWN) {down = false;}
-    }//GEN-LAST:event_formKeyReleased
-
     // Initiate the window
-    public static void main(String[] args) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Main().setVisible(true);
-            }
-        });
-    }
+    public static void main(String[] args) {EventQueue.invokeLater(() -> new Main().setVisible(true));}
 
     public void run() {
-        Graphics g = getBufferStrategy().getDrawGraphics();
-        // Objects:
-
-
-
-        //
         while(true) {
-            // Repaint the panel
-            backgroundPanel.repaint();
-            // Updates g
-            g = getBufferStrategy().getDrawGraphics();
-            // Clear the screen
-            g.clearRect(0, 0, getWidth(), getHeight());
-            // Runtime:
-
-
-
+            player.move(width, height);
             // Buffer in 16 milliseconds to approach the 60 fps
-            try {Thread.sleep(5);} catch (InterruptedException ex) {ex.printStackTrace();}
+            try {Thread.sleep(6);} catch (InterruptedException ex) {ex.printStackTrace();}
         }
     }
 }
